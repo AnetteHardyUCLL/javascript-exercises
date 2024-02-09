22b0596300f844692a0751491710a24068bbacc4
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-02-09 15:15:12 +0100

    update reverseString.js and test reverseString with a function that takes a string and returns its reversed version

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const strReversed = string.split("").reverse().join("");
+  return strReversed;
 };
 
 // Do not edit below this line
