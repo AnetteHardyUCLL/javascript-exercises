3acfe34c18c131446e830126b184070df0efc439
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-02-12 08:39:58 +0100

    update sumAll funcion in sumAll.js file so that given 2 integers returns the sum of every number between, and including, them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  if (
+    int1 < 0 ||
+    int2 < 0 ||
+    typeof int1 != "number" ||
+    typeof int2 != "number"
+  ) {
+    return "ERROR";
+  }
 
+  if (int2 < int1) {
+    temp = int1;
+    int1 = int2;
+    int2 = temp;
+  }
+
+  let sum = 0;
+  counter = int1;
+  while (counter <= int2) {
+    sum += counter;
+    counter++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
